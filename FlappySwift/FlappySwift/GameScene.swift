65f1399ba2e2f608f3f9a9d58b02accdd4d25ccc
Giordano Scalzo <giordano.scalzo@gmail.com>
2015-10-12 22:54:06 +0100

    Bird

--- FlappySwift/FlappySwift/GameScene.swift
+++ FlappySwift/FlappySwift/GameScene.swift
@@ -10,6 +10,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     private var screenNode: SKSpriteNode!
+    private var bird: Bird!
     private var actors: [Startable]!
     
     override func didMoveToView(view: SKView) {
@@ -19,10 +20,22 @@ class GameScene: SKScene {
         let sky = Background(textureNamed: "sky", duration:60.0).addTo(screenNode, zPosition: 0)
         let city = Background(textureNamed: "city", duration:20.0).addTo(screenNode, zPosition: 1)
         let ground = Background(textureNamed: "ground", duration:5.0).addTo(screenNode, zPosition: 2)
-        actors = [sky, city, ground]
+
+        bird = Bird(textureNames: ["bird1.png", "bird2.png"]).addTo(screenNode)
+        bird.position = CGPointMake(30.0, 400.0)
+        
+        actors = [sky, city, ground, bird]
         
         for actor in actors {
             actor.start()
         }
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        bird.update()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        bird.flap()
     }
 }
