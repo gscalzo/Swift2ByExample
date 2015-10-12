536f7bc9cffdd6af863731f562c365ad1f18e9bc
Giordano Scalzo <giordano.scalzo@gmail.com>
2015-10-12 22:16:53 +0100

    Menu

---
+++ FlappySwift/FlappySwift/GameViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GameViewController.swift
+//  FlappySwift
+//
+//  Created by Giordano Scalzo on 12/10/2015.
+//  Copyright (c) 2015 Giordano Scalzo. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+extension SKNode {
+    class func unarchiveFromFile(file : NSString) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file as String, ofType: "sks") {
+            let sceneData = try! NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe)
+            let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+}
+
+class GameViewController: UIViewController {
+    private let skView = SKView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        skView.frame = view.bounds
+        view.addSubview(skView)
+        
+        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
+            scene.size = skView.frame.size
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            skView.ignoresSiblingOrder = true
+            
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+}
