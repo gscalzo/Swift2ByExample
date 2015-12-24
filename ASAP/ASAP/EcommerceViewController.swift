0b88ccb6a5074c8e1cf62fb2222cea0189a3daf8
Giordano Scalzo <giordano.scalzo@gmail.com>
2015-12-24 15:34:00 +0000

    Product List

--- ASAP/ASAP/EcommerceViewController.swift
+++ ASAP/ASAP/EcommerceViewController.swift
@@ -8,9 +8,37 @@
 
 import UIKit
 
-class EcommerceViewController: UIViewController {
-
+class EcommerceViewController: UICollectionViewController {
+    
     static func instantiate() -> UIViewController {
         return UIStoryboard(name: "Ecommerce", bundle: nil).instantiateInitialViewController()!
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "ASAP"
+    }
+}
+
+extension EcommerceViewController {
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! ProductCollectionViewCell
+        
+        switch arc4random_uniform(4) {
+        case 0:
+            cell.backgroundColor = UIColor.redColor()
+        case 1:
+            cell.backgroundColor = UIColor.greenColor()
+        case 2:
+            cell.backgroundColor = UIColor.blueColor()
+        default:
+            cell.backgroundColor = UIColor.orangeColor()
+        }
+
+        return cell
     }
 }
